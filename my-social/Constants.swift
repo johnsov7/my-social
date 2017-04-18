692491f1548f5a89801bfc608f33fc8ef38352bf
vincent <myworkplace7@gmail.com>
2017-04-18 18:13:59 -0500

    Sign in ui custom class compete

---
+++ my-social/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  my-social
+//
+//  Created by Vincent A. Johnson Jr. on 4/18/17.
+//  Copyright © 2017 Vincent A. Johnson Jr. All rights reserved.
+//
+
+
+import UIKit
+
+let SHADOW_GRAY: CGFloat = 120.0 / 255.0
+
+
---
+++ my-social/FacyButton.swift
@@ -0,0 +1,23 @@
+//
+//  FacyButton.swift
+//  my-social
+//
+//  Created by Vincent A. Johnson Jr. on 4/18/17.
+//  Copyright © 2017 Vincent A. Johnson Jr. All rights reserved.
+//
+
+import UIKit
+
+class FacyButton: UIButton {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        layer.shadowColor = UIColor(red: SHADOW_GRAY, green: SHADOW_GRAY, blue: SHADOW_GRAY, alpha: 0.6).cgColor
+        layer.shadowOpacity = 0.8
+        layer.shadowRadius = 5.0
+        layer.shadowOffset = CGSize(width: 1.0, height: 1.0)
+        layer.cornerRadius = 2.0
+    }
+
+}
