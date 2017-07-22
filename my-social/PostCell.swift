7b69c52bb6112b8167b0df17f4e0b4e4e389abb0
vincent <myworkplace7@gmail.com>
2017-07-22 17:01:48 -0500

    Complete Firebase display

--- my-social/PostCell.swift
+++ my-social/PostCell.swift
@@ -10,15 +10,24 @@ import UIKit
 
 class PostCell: UITableViewCell {
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
     
     @IBOutlet weak var profileImg: UIImageView!
     @IBOutlet weak var username: UILabel!
     @IBOutlet weak var postImg: UIImageView!
     @IBOutlet weak var caption: UITextView!
     @IBOutlet weak var likeLbl: UILabel!
+    
+    var post: Post!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    func configureCell(post: Post) {
+        self.post = post
+        self.caption.text = post.caption
+        self.likeLbl.text = "\(post.likes)"
+    }
 
 }
